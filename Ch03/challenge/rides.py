411e7274963c8adc9d20a0cb7afb1b9895e0b2b8
momo3173 <137022016+momo3173@users.noreply.github.com>
2023-06-19 06:05:13 +0000

    Enter solution for Challenge: Find bad data

--- Ch03/challenge/rides.py
+++ Ch03/challenge/rides.py
@@ -4,7 +4,21 @@ import pandas as pd
 df = pd.read_csv('rides.csv')
 df
 # %%
-# Find out all the rows that have bad values
 # - Missing values are not allowed
+missing_values = df.isnull().any(axis=1)
+df[missing_values]
+
+#%%
 # - A plate must be a combination of at least 3 upper case letters or digits
+plate_combination = ~df["plate"].str.contains(r'[0-9]|[A-Z]{3,}', na=False)
+df[plate_combination]
+
+#%%
 # - Distance much be bigger than 0
+dist_b_zero = df["distance"] < 0
+df[dist_b_zero]
+
+#%%
+# Find out all the rows that have bad values
+all_bad = missing_values | plate_combination | dist_b_zero
+df[all_bad]
